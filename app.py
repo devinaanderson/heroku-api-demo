bef85053986d5438a1210fa6f82fd2c5ea38803f
devinaanderson <daa7896@gmail.com>
2018-12-07 09:16:40 -0700

    moved keys to secret file

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS 
+from secrets import return_uri
 
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://vjkhdjrydbigxj:260c8f99adf9b38164f8f3a817e97d9a53456f70fac1bd5984206071f3546ce7@ec2-184-72-239-186.compute-1.amazonaws.com:5432/dcg65lv1r0cnn6"
+app.config['SQLALCHEMY_DATABASE_URI'] = return_uri()
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -25,26 +26,46 @@ class User(db.Model):
 def home():
     return render_template('home.html')
 
-@app.route('/collections', methods=['POST'])
+# @app.route('/collections', methods=['POST', 'DELETE'])
+# def collections():
+#     email = None
+#     if request.method == 'POST':
+#         email = request.form['text']
+#         reg = User(email)
+#         db.session.add(reg)
+#         db.session.commit()
+#         return render_template('success.html')
+#     if request.method == 'DELETE':
+#         email = request.form['text']
+#         reg = User(email)
+#         db.session.delete(reg)
+#         db.session.commit()
+#         return render_template('success.html')
+#     return render_template('home.html')
+
+@app.route('/delete', methods=['POST'])
 def collections():
     email = None
     if request.method == 'POST':
-        email = request.form['text']
-        reg = User(email)
-        db.session.add(reg)
+        email = request.form['email']
+        obj=User.query.filter_by(email=email).first()
+        db.session.delete(obj)
         db.session.commit()
         return render_template('success.html')
-    # else if if request.method == 'DELETE':
-    #     email = request.form['text']
-    #     reg = User(email)
-    #     db.session.delete(reg)
-    #     db.session.commit()
-    return render_template('home.html')
+    return render_template("home.html")
 
 @app.route('/return_emails', methods=['GET'])
 def return_emails():
+    if request.method == 'DELETE':
+        email = request.form['text']
+        reg = User(email)
+        db.session.delete(reg)
+        db.session.commit()
+        return render_template('success.html')
     all_emails = db.session.query(User.email).all()
     return jsonify(all_emails)
+
+    
 
 if __name__ == '__main__':
     app.debug = True
